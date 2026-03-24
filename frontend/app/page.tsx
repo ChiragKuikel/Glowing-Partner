a7167d73cae4c2d02dc5ae1b6021fdeaaf138392
Chirag Kuinkel <chiragkuinkeldrive3@gmail.com>
2026-03-24 19:18:49 +0545

    philosophysection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from '../components/HeroSection'
 import Navbar from '@/components/navbar';
+import PhilosophyPage from '@/components/Philosophy';
 import PartnerRibbon from '@/components/Swiper';
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <PartnerRibbon />
+      <PhilosophyPage />
     </div>
   );
 }
